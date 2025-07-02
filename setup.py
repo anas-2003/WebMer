55181052d3e637c7364a6497722daf414b6d1fb9
Anas Erami <70097211+anas-2003@users.noreply.github.com>
2025-07-03 00:07:59 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+# Read the contents of your requirements.txt file
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='webmer',
+    version='5.0.0',
+    author='Anas Erami',
+    author_email='anaserami17@gmail.com', 
+    description='An Advanced, AI-Driven Offensive Security Platform for Web Applications & APIs.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/anas-2003/webmer', 
+    packages=find_packages(),
+    py_modules=['webmer'], 
+    install_requires=requirements,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Security',
+    ],
+    python_requires='>=3.8',
+    entry_points={
+        'console_scripts': [
+            'webmer = webmer:main',
+        ],
+    },
+)
